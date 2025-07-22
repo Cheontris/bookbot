1020e75c9a6cbcad642d5994a1f24c2477d5aeb0
Cheontris <noah.hughes116@gmail.com>
2025-07-22 19:48:42 -0400

    imported sys to handle file reading

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from stats import get_text_word_count
+from stats import get_character_count
+from stats import dictionary_sort
+import sys
 def main():
-    with open("./books/frankenstein.txt") as f:
+    if(not len(sys.argv) == 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_string = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_string}")
+    with open(book_string) as f:
+        print("----------- Word Count ----------")
         get_text_word_count(f.read())
+    with open(book_string) as f:
+        sort = dictionary_sort(get_character_count(f.read()))
+        print ("--------- Character Count -------")
+        for pair in sort:
+            if pair["char"].isalpha():
+                print(f"{pair["char"]}: {pair["num"]}")
+        print("============= END ===============")
 def get_book_text(file):
     print (file.read())
 
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
 def get_text_word_count(book_text):
     word_count = len(book_text.split())
-    print (f"{word_count} words found in the document")
+    print (f"Found {word_count} total words")
+def get_character_count(book_text):
+    chars = []
+    counts = []
+    char_counts = {}
+    for c in range (len(book_text)):
+        if book_text[c].lower() not in chars:
+            chars.append(book_text[c].lower())
+            counts.append(1)
+        else:
+            counts[chars.index(book_text[c].lower())] += 1
+    #Testing Lists:
+    #print (chars)
+    #print (counts)
+    for c in range (len(chars)):
+        char_counts[chars[c]] = counts[c]
+    return (char_counts)
+def dictionary_sort(dictionary):
+    sorted_dictionary = []
+    for c, n in dictionary.items():
+        sorted_dictionary.append({"char" : c , "num" : n})
+    sorted_dictionary.sort(reverse=True, key=sort_on)
+    return sorted_dictionary
+def sort_on(items):
+    return items["num"]
